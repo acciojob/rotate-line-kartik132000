a62df83749fd38f49123ba048a6b21df40ba8a39
kartik132000 <sujeetkumarsen37@gmail.com>
2025-01-31 11:33:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+ let angle = 0;
+        const line = document.getElementById('line');
+
+        function rotateLine() {
+            angle += 2;
+            line.style.transform = `rotate(${angle}deg)`;
+            setTimeout(rotateLine, 20);
+        }
+
+        rotateLine();
